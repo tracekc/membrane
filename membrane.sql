4c4e821169820e589c5267c32dfb846380c9022f
tracekc <kirancnair@gmail.com>
2020-08-24 19:02:11 -0700

    Update membrane.sql

--- membrane.sql
+++ membrane.sql
@@ -2,12 +2,12 @@
 CREATE TABLE `team` (
   `team_name_id` varchar(225) NOT NULL,
   `name` varchar(255) NOT NULL,
-  'is_individual' boolean DEFAULT TRUE,
+  `is_individual` boolean DEFAULT TRUE,
   `creation_date` date DEFAULT NULL,
   `pageurl` varchar(2083) DEFAULT NULL,
-  'voting_freq' int DEFAULT NULL,
-  'last_voted' date DEFAULT NULL,
-  'size' int DEFAULT 6,
+  `voting_freq` int DEFAULT NULL,
+  `last_voted` date DEFAULT NULL,
+  `size` int DEFAULT 6,
   PRIMARY KEY (`team_name_id`)
 )
 
@@ -17,7 +17,7 @@ CREATE TABLE `relationships` (
   `relationship_id` varchar(225) NOT NULL,
   `parent_id` varchar(225) NOT NULL,
   `child_id` varchar(255) NOT NULL,
-  'is_leader' boolean DEFAULT FALSE,
+  `is_leader` boolean DEFAULT FALSE,
   `creation_date` date DEFAULT NULL,
   PRIMARY KEY (`relationship_id`),
   FOREIGN KEY (`parent_id`) REFERENCES `team` (`team_name_id`),
